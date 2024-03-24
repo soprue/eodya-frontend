7ba2b838a6f69dd54127c9949fbfa0fb8b4c8edd
Kim Ji Yu <spbabo97@naver.com>
2024-03-25 01:56:46 +0900

    init : 프로젝트 초기세팅
    Redux-toolkit, tailwindcss, typescript, react-router-dom, 필요한 폴더구조 생성

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
+import { useAppSelector } from './store/hooks';
+
+function App() {
+
+  //테스트용으로 redux 넣어놨습니당
+  const test = useAppSelector((state)=>state.test.value);
+
+  return (
+    <Routes>
+      <Route path='/' 
+        element={
+          <>
+            <div className="App text-center h-screen flex items-center justify-center flex-col">
+              <img className='mx-auto' src={logo} alt="" width={200} /><br/>
+              <p className='font-bold'>{test} 합시당!</p>
+            </div>
+          </>
+        } 
+      />
+    </Routes>
+  );
+}
+
+export default App;
