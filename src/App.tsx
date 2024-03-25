c5c39b2d019a00a04cb928fc5fb546f489fa757a
Gahee Kim <soprue@naver.com>
2024-03-25 23:55:31 +0900

    Chore: tailwind config setting

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import logo from './logo.svg';
 import { useAppSelector } from './store/hooks';
 
 function App() {
-
   //테스트용으로 redux 넣어놨습니당
-  const test = useAppSelector((state)=>state.test.value);
+  const test = useAppSelector((state) => state.test.value);
 
   return (
     <Routes>
-      <Route path='/' 
+      <Route
+        path="/"
         element={
           <>
-            <div className="App text-center h-screen flex items-center justify-center flex-col">
-              <img className='mx-auto' src={logo} alt="" width={200} /><br/>
-              <p className='font-bold'>{test} 합시당!</p>
+            <div className="App text-center h-screen flex items-center justify-center flex-col font-pretendard">
+              <img className="mx-auto" src={logo} alt="" width={200} />
+              <br />
+              <p className="font-bold">{test} 합시당!</p>
             </div>
           </>
-        } 
+        }
       />
     </Routes>
   );
