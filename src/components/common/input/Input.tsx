daf0b50f87d2bbf4ec8b0bd5525196d693e0b032
Kim Ji Yu <spbabo97@naver.com>
2024-03-26 15:35:16 +0900

    Feat : Input창 value값 추가시 아이콘 색상 변경

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,20 +1,29 @@
-// input창에 입력부분과 아이콘 클릭시 submit이 들어가게 컴포넌트를 구성했습니다.
+/*  
+    input창에 입력부분과 
+    검색 버튼 아이콘에는 기본적으로 type이 submit으로 들어가 있습니다.
+    value값이 수정이 되면 search 아이콘의 색상이 변동이 됩니다.
+*/
+
+import { ReactComponent as Search} from "../../../assets/image/icon/search.svg";
+
 export default function Input({
     name,
     id,
     type,
     placeholder,
+    value,
     maxLength,
     className,
     onChange,
     onInput
 } : React.InputHTMLAttributes<HTMLInputElement>) {
+
   return (
     <div
         className={`${className ? className : ""} w-full relative rounded-[10px] h-[46px] font-pretendard overflow-hidden`}
     >
         <input
-            className="px-5 min-w-0 w-full h-full text-base tracking-[-0.02em] leading-4 outline-none"
+            className="px-5 min-w-0 w-full h-full text-base tracking-[-0.02em] leading-4 outline-none placeholder:text-gray-300"
             onInput={onInput}
             onChange={onChange}
             name={name}
@@ -22,11 +31,14 @@ export default function Input({
             maxLength={maxLength}
             type={type}
             placeholder={placeholder}
+            value={value}
         ></input>
         <button 
             className="absolute right-5 top-1/2 -translate-y-1/2"
             type="submit"
-        >검색</button>
+        ><Search className={`${value !== "" ? "fill-gray-900" : "fill-gray-300"}`}/></button>
     </div>
   )
+
+
 }
