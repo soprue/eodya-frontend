ab74aad8133160641152e02d804808e7e7f3bd48
Kim Ji Yu <spbabo97@naver.com>
2024-03-26 18:11:13 +0900

    Chore : 카카오 Map API 적용

--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,22 +1,41 @@
-import Btn from "../../components/common/btn/Btn";
+import { Map, MapMarker, MarkerClusterer } from "react-kakao-maps-sdk";
 import Input from "../../components/common/input/Input";
 import Navigation from "../../components/common/menu/Navigation";
-import TopBar from "../../components/common/menu/TopBar";
+import {ReactComponent as BookmarkSVG} from "../../assets/image/icon/bookmark.svg";
+import {ReactComponent as LocationSVG} from "../../assets/image/icon/location.svg";
 
 export default function Main() {
 
+  const onClick = ()=>{
+    
+  }
+
   return (
-    <main className="bg-black h-dvh">
-        <Navigation/>
-        <Btn type="button">내용</Btn>
-        <TopBar>
-          <p className="text-center absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">스팟 등록</p>
-        </TopBar>
-        <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">화이팅!</p>
-        <Input 
-          placeholder="입력"
-        />
+    <main className="h-screen">
+      <div className="absolute z-50 w-full top-[30px] px-4">
+        <Input type="text" placeholder="장소를 검색해 보세요"/>
+        <div className="w-10 h-10 rounded-lg bg-white mt-3 flex items-center justify-center">
+          <BookmarkSVG className="fill-primary"/>
+        </div>
+      </div>
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }}
+        style={{ width: "100%", height : "100%" }}
+        level={5}
+      >
+        <MarkerClusterer
+          averageCenter={true}
+          minLevel={10}
+        >
+          <MapMarker
+            position={{ lat: 33.55635, lng: 126.795841 }}
+          />
+        </MarkerClusterer>
+      </Map>
+      <button className="w-11 h-11 rounded-full flex items-center justify-center absolute z-50 bg-white shadow-[0px_0px_3px_0px_#00000040] bottom-[90px] left-4">
+        <LocationSVG/>
+      </button>
+      <Navigation/>
     </main>
   )
 }
-
