4055fd03945b893ca198738a9c597b25899d9b3e
Gahee Kim <soprue@naver.com>
2024-03-26 16:22:21 +0900

    Style: tailwind class order update

--- src/components/common/menu/Navigation.tsx
+++ src/components/common/menu/Navigation.tsx
@@ -5,28 +5,35 @@ import person from "../../../assets/image/icon/person.svg";
 
 // 하단 네비게이션바
 export default function Navigation() {
-
   return (
-    <nav className="absolute bottom-0 h-[70px] w-full flex justify-between text-[11px] font-pretendard font-semibold text-center text-primary bg-white">
-        <Link to={'/'} className="flex-1 h-full flex items-center justify-center">
-            <dl>
-                <dt><img className="mx-auto" src={home} alt="home 버튼" /></dt>
-                <dd className="mt-[2px]">Home</dd>
-            </dl>
-        </Link>
-        <Link to={'/'} className="flex-1 h-full flex items-center justify-center">
-            <dl>
-                <dt><img className="mx-auto" src={edit} alt="등록하기 버튼" /></dt>
-                <dd className="mt-[2px]">등록하기</dd>
-            </dl>
-        </Link>
-        <Link to={'/'} className="flex-1 h-full flex items-center justify-center">
-            <dl>
-                <dt><img className="mx-auto" src={person} alt="마이페이지 버튼" /></dt>
-                <dd className="mt-[2px]">마이페이지</dd>
-            </dl>
-        </Link>
+    <nav className="absolute bottom-0 flex h-[70px] w-full justify-between bg-white text-center font-pretendard text-[11px] font-semibold text-primary">
+      <Link to={"/"} className="flex h-full flex-1 items-center justify-center">
+        <dl>
+          <dt>
+            <img className="mx-auto" src={home} alt="home 버튼" />
+          </dt>
+          <dd className="mt-[2px]">Home</dd>
+        </dl>
+      </Link>
+      <Link
+        to={"/new"}
+        className="flex h-full flex-1 items-center justify-center"
+      >
+        <dl>
+          <dt>
+            <img className="mx-auto" src={edit} alt="등록하기 버튼" />
+          </dt>
+          <dd className="mt-[2px]">등록하기</dd>
+        </dl>
+      </Link>
+      <Link to={"/"} className="flex h-full flex-1 items-center justify-center">
+        <dl>
+          <dt>
+            <img className="mx-auto" src={person} alt="마이페이지 버튼" />
+          </dt>
+          <dd className="mt-[2px]">마이페이지</dd>
+        </dl>
+      </Link>
     </nav>
-  )
-  
+  );
 }
