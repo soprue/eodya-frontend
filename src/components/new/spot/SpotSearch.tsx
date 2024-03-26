62efcb63a36dcbe10c1353296183a6566c5d7e2b
Gahee Kim <soprue@naver.com>
2024-03-26 23:58:53 +0900

    Feat: SpotSearch 레이아웃 설계

--- src/components/new/spot/SpotSearch.tsx
+++ src/components/new/spot/SpotSearch.tsx
@@ -1,5 +1,6 @@
 import { CustomOverlayMap, Map } from "react-kakao-maps-sdk";
 
+import { ReactComponent as Edit } from "../../../assets/image/icon/edit.svg";
 import Input from "../../common/input/Input";
 import Btn from "../../common/btn/Btn";
 import BasicMarker from "../../common/marker/BasicMarker";
@@ -24,7 +25,16 @@ function SpotSearch({ onNext }: SpotSearchProps) {
       <div className="absolute bottom-0 z-10 flex h-[200px] w-full flex-col justify-between bg-white px-4 py-7">
         <p className="text-xl font-semibold">이 곳을 방문했어요!</p>
 
-        <Input className="bg-gray-100"></Input>
+        <div
+          className={`flex h-[46px] w-full cursor-pointer items-center justify-between overflow-hidden rounded-[10px] bg-gray-100 px-5`}
+        >
+          <input
+            className="h-full w-[calc(100%-50px)] min-w-0 cursor-pointer bg-transparent text-base font-semibold leading-4 tracking-[-0.02em] outline-none placeholder:text-gray-300"
+            value=""
+            readOnly
+          />
+          <Edit className="fill-gray-950" />
+        </div>
 
         <Btn onClick={onNext}>이 위치 등록하기</Btn>
       </div>
