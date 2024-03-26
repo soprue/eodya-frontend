a50115e1accebf4958f1d564c6f94851d6765ddb
Kim Ji Yu <spbabo97@naver.com>
2024-03-26 13:37:25 +0900

    Design : 기본 레이아웃 수정

--- src/components/common/menu/Navigation.tsx
+++ src/components/common/menu/Navigation.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import home from "../../../assets/image/icon/home.svg";
+import edit from "../../../assets/image/icon/edit.svg";
+import person from "../../../assets/image/icon/person.svg";
 
 // 하단 네비게이션바
 export default function Navigation() {
@@ -7,19 +10,19 @@ export default function Navigation() {
     <nav className="absolute bottom-0 h-[70px] w-full flex justify-between text-[11px] font-pretendard font-semibold text-center text-primary bg-white">
         <Link to={'/'} className="flex-1 h-full flex items-center justify-center">
             <dl>
-                <dt>아이콘</dt>
+                <dt><img className="mx-auto" src={home} alt="home 버튼" /></dt>
                 <dd className="mt-[2px]">Home</dd>
             </dl>
         </Link>
         <Link to={'/'} className="flex-1 h-full flex items-center justify-center">
             <dl>
-                <dt>아이콘</dt>
+                <dt><img className="mx-auto" src={edit} alt="등록하기 버튼" /></dt>
                 <dd className="mt-[2px]">등록하기</dd>
             </dl>
         </Link>
         <Link to={'/'} className="flex-1 h-full flex items-center justify-center">
             <dl>
-                <dt>아이콘</dt>
+                <dt><img className="mx-auto" src={person} alt="마이페이지 버튼" /></dt>
                 <dd className="mt-[2px]">마이페이지</dd>
             </dl>
         </Link>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 // 모바일 기준으로 만들기 때문에 max-width을 720px 로 일단 잡아뒀습니다.
 export default function Layout() {
   return (
-    <div className="mx-auto max-w-3xl relative">
+    <div className="mx-auto max-w-lg relative">
         <Outlet/>
     </div>
   )
