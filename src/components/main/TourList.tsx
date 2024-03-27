c6c2bf58128215ccfa166b9934ec86046cedcdf3
Kim Ji Yu <spbabo97@naver.com>
2024-03-28 05:37:22 +0900

    Feat : 근처의 명소 모달창 컴포넌트화

--- src/components/main/TourList.tsx
+++ src/components/main/TourList.tsx
@@ -8,46 +8,60 @@ import RankModal from "./Modal/RankModal";
 
 export function TourList(){
 
-    return (
-      <>
-        <div className={`bg-white rounded-t-[10px] rounded-r-[10px] pt-7 font-pretendard h-[calc(100vh-70px)] flex flex-col `}>
+  const [isOpen,setIsOpenl] = useState(false);
 
-          <div className="flex justify-between items-center px-4 flex-none">
-            <h2 className="text-xl tracking-[-0.02em] font-semibold">근처의 명소</h2>
-            <button 
-              className="flex items-center text-[13px] tracking-[-0.02em] font-medium"
-            >
-              랭킹순 <More className="fill-gray-800"/>
-            </button>
-          </div>
+  const onOpen = ()=>{
+    setIsOpenl(true);
+  }
 
-          <div className="overflow-y-auto h-full scrollbar-hide">
-            {
-              [
-                {
-                  placeState : "만개",
-                  image : [0,1]
-                },
-                {
-                  placeState : "개화",
-                  image : [0]
-                },
-                {
-                  placeState : "내년에 만나요",
-                  image : [0,1]
-                },
-                {
-                  placeState : "개화",
-                  image : [0]
-                }
-              ].map((e,i)=>(
-                <ListLayout item={e} key={i}/>
-              ))
-            }
-          </div>
+  const onClose = ()=>{
+    setIsOpenl(false);
+  }
 
+  return (
+    <>
+      <div className={`bg-white rounded-t-[10px] rounded-r-[10px] pt-7 font-pretendard h-[calc(100vh-70px)] flex flex-col `}>
+
+        <div className="flex justify-between items-center px-4 flex-none">
+          <h2 className="text-xl tracking-[-0.02em] font-semibold">근처의 명소</h2>
+          <button 
+            className="flex items-center text-[13px] tracking-[-0.02em] font-medium"
+            onClick={onOpen}
+          >
+            랭킹순 <More className="fill-gray-800"/>
+          </button>
         </div>
-        
-      </>
-    )
+
+        <div className="overflow-y-auto h-full scrollbar-hide">
+          {
+            [
+              {
+                placeState : "만개",
+                image : [0,1]
+              },
+              {
+                placeState : "개화",
+                image : [0]
+              },
+              {
+                placeState : "내년에 만나요",
+                image : [0,1]
+              },
+              {
+                placeState : "개화",
+                image : [0]
+              }
+            ].map((e,i)=>(
+              <ListLayout item={e} key={i}/>
+            ))
+          }
+        </div>
+
+      </div>
+
+      <RankModal isOpen={isOpen} onClose={onClose}/>
+      
+    </>
+  )
+  
 }
