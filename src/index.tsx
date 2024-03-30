a27aab9fdc65ddf77d060a9210c674ca5e4b88f3
Kim Ji Yu <spbabo97@naver.com>
2024-03-30 20:02:38 +0900

    Fix : develop 에서 병합 오류 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 import Modal from 'react-modal';
 import { persistor, store } from './store/store';
