6b4d1f9f67695a7e97544a2c825cb024f40cfa6b
Gahee Kim <soprue@naver.com>
2024-03-31 11:26:26 +0900

    Fix: import 오류 수정

--- src/components/main/ListLayout.tsx
+++ src/components/main/ListLayout.tsx
@@ -1,22 +1,30 @@
-import { ReviewDetailList } from "../../store/features/main/tourList/tourPlace"
-import { InfoPlaceType } from "../../types/InfoPlaceType"
-import { BookMarkBtn } from "../common/btn/BookMarkBtn"
-import FlowerTag from "../common/tag/FlowerTag"
+import { InfoPlaceType } from "../../types/InfoPlaceType";
+import { BookMarkBtn } from "../common/btn/BookMarkBtn";
+import FlowerTag from "../common/tag/FlowerTag";
 
 // 스팟선택
-export const ListLayout = ({item,onClick} : {item : InfoPlaceType,onClick? : React.MouseEventHandler<HTMLDivElement>}) => {
-
+export const ListLayout = ({
+  item,
+  onClick,
+}: {
+  item: InfoPlaceType;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}) => {
   return (
     <div className="bg-white p-4 font-pretendard" onClick={onClick}>
-      
       <div className="relative">
-        <div className="absolute top-[10px] left-[10px] z-20 leading-none">
-          <FlowerTag placeState={item.placeStatus}/>
+        <div className="absolute left-[10px] top-[10px] z-20 leading-none">
+          <FlowerTag placeState={item.placeStatus} />
         </div>
-        <div className={`grid grid-cols-${item.image.length} gap-1 rounded-lg overflow-hidden`}>
-          
-          <div className="relative w-full h-40">
-            <img className="absolute top-0 left-0 w-full h-full object-cover object-center" src={item.image} alt={`${item.name} 이미지`} />
+        <div
+          className={`grid grid-cols-${item.image.length} gap-1 overflow-hidden rounded-lg`}
+        >
+          <div className="relative h-40 w-full">
+            <img
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
+              src={item.image}
+              alt={`${item.name} 이미지`}
+            />
           </div>
 
           {/* {
@@ -26,22 +34,21 @@ export const ListLayout = ({item,onClick} : {item : InfoPlaceType,onClick? : Rea
               </div>
             ))
           } */}
-
         </div>
       </div>
 
-      <div className="mt-3 flex justify-between items-start">
+      <div className="mt-3 flex items-start justify-between">
         <div className="leading-none tracking-custom">
           <dl>
             <dt className="text-base font-bold text-gray-950">{item.name}</dt>
-            <dd className="mt-1 text-sm font-normal leading-[21px]">{item.addressDetail}</dd>
+            <dd className="mt-1 text-sm font-normal leading-[21px]">
+              {item.addressDetail}
+            </dd>
           </dl>
           {/* <p className="text-[13px] mt-1 leading-[13px] text-info-300 font-semibold">820m</p> */}
         </div>
-        <BookMarkBtn placeId={""} status={false}/>
+        <BookMarkBtn placeId={""} status={false} />
       </div>
-
     </div>
-  )
-  
-}
+  );
+};
--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useEffect, useState } from "react";
-import { Map} from "react-kakao-maps-sdk";
 import { useDispatch } from "react-redux";
 import { Map, MarkerClusterer } from "react-kakao-maps-sdk";
 
@@ -14,33 +13,26 @@ import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { useWatchLocation } from "../../hook/mapLocation/useWatchLocation";
 import UserMarker from "../../components/common/marker/UserMarker";
 import { change as TourChange } from "../../store/features/main/tourList/openSlice";
-import {
-  getMarker,
-} from "../../store/features/main/marker/markerSlice";
+import { getMarker } from "../../store/features/main/marker/markerSlice";
 import SpotIntro from "../../components/main/SpotIntro";
 import { getPlace } from "../../store/features/main/spotInfo/InfoPlace";
 import { getTourPlace } from "../../store/features/main/tourList/tourPlace";
 
 import { hide } from "../../store/features/main/map/tourClick";
 import { spotHide, spotShow } from "../../store/features/main/map/spotClick";
-import { SpotIntro } from "../../components/main/SpotIntro";
-import { getPlace } from "../../store/features/main/spotInfo/InfoPlace";
-import { getTourPlace } from "../../store/features/main/tourList/tourPlace";
 
 export default function Main() {
   const dispatch = useAppDispatch();
-  const {userInfo} = useAppSelector(state=>state.auth);
-  const [bookMark,setBookMark] = useState(false);
+  const { userInfo } = useAppSelector((state) => state.auth);
+  const [bookMark, setBookMark] = useState(false);
 
   // 마커 fetch
-  const {markers} = useAppSelector(state=>state.mainMarker);
+  const { markers } = useAppSelector((state) => state.mainMarker);
   // 마커 가져오기
-  useEffect(()=>{ 
-
-    if(!userInfo) return;
-    dispatch(getMarker(userInfo.token)); 
-
-  },[bookMark,userInfo]);
+  useEffect(() => {
+    if (!userInfo) return;
+    dispatch(getMarker(userInfo.token));
+  }, [bookMark, userInfo]);
 
   // 지도 초기위치 설정
   const [state, setState] = useState({
@@ -61,46 +53,51 @@ export default function Main() {
       return alert(error.message);
     }
 
-    if(!center) return;
-    setState({center,isPanto : true});
-
-  },[]);
-  useEffect(()=>{ getPostion(); setTimeout(()=>{getTourList()},2000) },[]);
+    if (!center) return;
+    setState({ center, isPanto: true });
+  }, []);
+  useEffect(() => {
+    getPostion();
+    setTimeout(() => {
+      getTourList();
+    }, 2000);
+  }, []);
 
   // 현재 위치를 토대로 근처의 명소 가져오기
-  const getTourList = ()=>{
-
-    if(!userInfo) return;
+  const getTourList = () => {
+    if (!userInfo) return;
 
     const geocoder = new kakao.maps.services.Geocoder();
 
-    geocoder.coord2Address(state.center.lng,state.center.lat, (result,status)=>{
-      if(status === kakao.maps.services.Status.OK){
-        dispatch(
-          getTourPlace({
-            token : userInfo.token, 
-            address : result[0].address.region_1depth_name,
-            page : 1 
-          })
-        );
-      }else{
-        dispatch(TourChange(false));
-      }
-    })
-
-  }
+    geocoder.coord2Address(
+      state.center.lng,
+      state.center.lat,
+      (result, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          dispatch(
+            getTourPlace({
+              token: userInfo.token,
+              address: result[0].address.region_1depth_name,
+              page: 1,
+            }),
+          );
+        } else {
+          dispatch(TourChange(false));
+        }
+      },
+    );
+  };
 
   // 현재위치 watch
-  const {location} = useWatchLocation();
+  const { location } = useWatchLocation();
 
   return (
     <>
       <main className="relative h-screen overflow-hidden">
-        
         {/* 검색버튼 */}
-        <div className="absolute z-50 w-full top-[30px] px-4">
-          <Input type="text" placeholder="장소를 검색해 보세요"/>
-          <MainBookMarkBtn bookMark={bookMark} setBookMark={setBookMark}/>
+        <div className="absolute top-[30px] z-50 w-full px-4">
+          <Input type="text" placeholder="장소를 검색해 보세요" />
+          <MainBookMarkBtn bookMark={bookMark} setBookMark={setBookMark} />
           {/* 임시 로그아웃 버튼 */}
           <button
             onClick={() => dispatch(logout())}
@@ -141,30 +138,28 @@ export default function Main() {
           )}
 
           {/* 마커 */}
-          {
-            markers.map((e,i)=>(
-              <BlossomMarker
-                key={i+e.x+e.y}
-                position={{ lat: e.x, lng: e.y }}
-                onClick={()=>{
-                  if(!userInfo) return;
-                  // info 데이터
-                  dispatch(hide());
-                  dispatch(spotShow());
-                  dispatch(getPlace({token : userInfo.token, placeId : e.placeId}))
-                }}
-              />
-            ),
-          )}
-
+          {markers.map((e, i) => (
+            <BlossomMarker
+              key={i + e.x + e.y}
+              position={{ lat: e.x, lng: e.y }}
+              onClick={() => {
+                if (!userInfo) return;
+                // info 데이터
+                dispatch(hide());
+                dispatch(spotShow());
+                dispatch(
+                  getPlace({ token: userInfo.token, placeId: e.placeId }),
+                );
+              }}
+            />
+          ))}
         </Map>
 
         {/* 마커 관련 명소 */}
-        <SpotIntro getPostion={getPostion}/>
+        <SpotIntro getPostion={getPostion} />
 
         {/* 네비게이션바 */}
-        <Navigation/>
-
+        <Navigation />
       </main>
       <SpotView />
     </>
